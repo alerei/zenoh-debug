bdda755f63998a7e68e0c8796729592292e13861
Alexander Reichert <alex@flecs.tech>
2023-04-15 16:03:43 +0200

    Initial project with prebuilt zenoh-c

---
+++ external/out/include/zenoh.h
@@ -0,0 +1,36 @@
+#ifndef ZENOH_H
+#define ZENOH_H
+
+#include <assert.h>
+#include <stdint.h>
+
+#include "zenoh_configure.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define ZENOH_C "0.7.0"
+#define ZENOH_C_MAJOR 0
+#define ZENOH_C_MINOR 7
+#define ZENOH_C_PATCH 0
+
+#ifdef _MSC_VER
+#define ALIGN(n) __declspec(align(n))
+#else
+#define ALIGN(n) __attribute__((aligned(n)))
+#endif
+
+#include "zenoh_configure.h"
+
+// clang-format off
+// include order is important
+#include "zenoh_concrete.h"
+#include "zenoh_commons.h"
+// clang-format on
+
+#ifdef __cplusplus
+}
+#endif
+#include "zenoh_macros.h"
+#endif
